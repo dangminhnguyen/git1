701e6c6d6b1d0b6ed6cf67ad19805cf7853b27f9
nguyen dang <dangnguyenminh171@gmail.com>
2019-07-02 13:54:08 +0700

    master cm2

--- git1/ViewController.swift
+++ git1/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("dangminhnguye")
+        print("dangminhnguye111")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
